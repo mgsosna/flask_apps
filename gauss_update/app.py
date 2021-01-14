97c464cc2d5ae20cd33cdf2bec3faa2093d4dc05
mgsosna <matt.g.sosna@gmail.com>
2021-01-13 21:20:13 -0500

    Connect JS to numpy

--- gauss_update/app.py
+++ gauss_update/app.py
@@ -1,1 +1,23 @@
-from flask import Flask, render_template
+import numpy as np
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/normal", methods=['POST'])
+def normal():
+
+    input_dict = request.get_json()
+    print(f"Contents are: {input_dict}")
+
+    # Convert all keys to int
+    for key in input_dict.keys():
+        input_dict[key] = int(input_dict[key])
+
+    return jsonify(list(np.random.normal(**input_dict).round(3)))
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- gauss_update/static/script.js
+++ gauss_update/static/script.js
@@ -1,43 +1,76 @@
-// Initialize the Plotly plot with a generic histogram
-function initPlot(){
+function createPlot(x) {
 
-    var x = [];
-    for(var i=0; i<500; i++){
-        x[i] = Math.random();
-    };
-
-    var trace = {
-        x: x,
-        type: 'histogram'
-    };
+    var trace = {x: x,
+                 type: 'histogram',
+                 xbins: {start: 10, end: 100, step: 2}};
 
     var data = [trace];
 
     var layout = {
         title: "Normal distribution",
-        xaxis: {range: [10, 100]}
+        xaxis: {range: [10, 100]},
+        yaxis: {range: [0, 200]}
+
     };
 
     Plotly.newPlot("plot", data, layout);
 }
 
+// Get data
+function updateData() {
+
+    var inputs = {
+        loc: d3.select("#mean").property("value"),
+        scale: d3.select("#sd").property("value"),
+        size: 1000
+    };
+
+    json = JSON.stringify(inputs);
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/normal");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = handleData;
+    xhr.send(json);
+
+    function handleData() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var newData = JSON.parse(xhr.responseText.split(', '));
+            console.log(newData);
+            createPlot(newData);
+        }
+    }
+}
+
 // Update labels with value from sliders
-function updateLabels() {
-
-    var mean = document.getElementById("mean").value;
+function updateMeanLabel() {
+    var mean = d3.select("#mean").property("value");
     d3.select("#mean_label").html(`Mean: <strong>${mean}</strong>`);
+}
 
-    var sd = document.getElementById("sd").value;
+function updateSdLabel() {
+    var sd = d3.select("#sd").property("value");
     d3.select("#sd_label").html(`Standard deviation: <strong>${sd}</strong>`);
 }
 
 // Initialize page
 function init() {
-    initPlot();
-    updateLabels();
+    updateData();
+    updateMeanLabel();
+    updateSdLabel();
+}
+
+function handleMean() {
+    updateMeanLabel();
+    updateData();
+}
+
+function handleSd() {
+    updateSdLabel();
+    updateData();
 }
 
 init();
 
-d3.select("#mean").on("input", updateLabels);
-d3.select("#sd").on("input", updateLabels);
+d3.select("#mean").on("input", handleMean);
+d3.select("#sd").on("input", handleSd);
